7b770a3e55ce0d65979ac1db53361be16bfebf4d
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-06-08 17:05:13 -0400

    comment out move commands

--- disable-ad-users.ps1
+++ disable-ad-users.ps1
@@ -12,7 +12,7 @@ Set-ADObject -Identity $user -Description "disabled $($time)"
 
 $userDN=(Get-ADUser $users).DistinguishedName
 
-Get-ADUser -Identity $userDN | Move-ADObject -TargetPath "OU=Disabled Users,OU=SBSUsers,OU=Users,OU=MyBusiness,DC=Marone,DC=local"
+#Get-ADUser -Identity $userDN | Move-ADObject -TargetPath "OU=Disabled Users,OU=SBSUsers,OU=Users,OU=MyBusiness,DC=Marone,DC=local"
 
 write-host "user $($user) has been disabled on $($time)"
 
