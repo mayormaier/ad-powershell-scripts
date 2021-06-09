201d2b0b8e887654cc2bb66c1aa72344e5d8cb31
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-06-09 10:54:56 -0400

    upgrade to "delete computers" script

---
+++ disable-ad-computers.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module ActiveDirectory
+$comps=Get-Content C:\users\nst\Desktop\disablecomputers.txt
+$time=Get-Date
+#$targetOU="OU=Disabled Computers,OU=SBSComputers,OU=Computers,OU=MyBusiness,DC=Marone,DC=local"
+ 
+
+ForEach ($comp in $comps)
+{
+
+if ($comp.Trim()) {
+
+    Get-ADComputer $comp.Trim() | Disable-ADAccount #-PassThru | Move-ADObject -TargetPath $targetOU
+    
+    }
+
+Set-ADComputer -Identity $comp -Description "disabled on $($time)"
+
+write-host "computer $($comp) has been disabled on $($time)"
+
+}
