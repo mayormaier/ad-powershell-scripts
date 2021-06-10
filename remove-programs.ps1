569f245db30370db9eae6a3d3c21e06b1a7af665
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-06-10 11:50:26 -0400

    uninstall files script

---
+++ remove-programs.ps1
@@ -0,0 +1,14 @@
+﻿#Import-Module hsg*6
+
+$programs=Get-Content C:\users\Andrew\Documents\powershell-scripts\uninstallprog.txt
+
+ForEach ($prog in $programs)
+
+{
+Write-Host "uninstalling $($prog)..."
+
+$application = Get-WmiObject -Class Win32_Product -Filter "Name LIKE '%$($prog)%'"
+$application.Uninstall() 
+
+if ($?) {Write-Host "uninstall of $($prog) succeeded"}
+}
