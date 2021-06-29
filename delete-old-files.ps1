472b9c389bc5d474bd8d6dd7c9e1671cd4e7922b
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-06-29 10:49:12 -0400

    initial commit

---
+++ delete-old-files.ps1
@@ -0,0 +1,19 @@
+﻿$Folder = $args[0]
+$Days = $args[1]
+
+#Delete files older than x months (user cli args)
+Get-ChildItem $Folder -Recurse -Force -ea 0 |
+? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays($Days)} |
+ForEach-Object {
+   $_ | del -Force
+   Write-Host "Deleting item " + $_.FullName
+}
+
+#Delete empty folders and subfolders
+Get-ChildItem $Folder -Recurse -Force -ea 0 |
+? {$_.PsIsContainer -eq $True} |
+? {$_.getfiles().count -eq 0} |
+ForEach-Object {
+    $_ | del -Force
+    Write-Host "Deleting item " + $_.FullName
+}
