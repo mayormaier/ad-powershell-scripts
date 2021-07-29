8293e7bf9cf110b9ac294dc2fc0767f4a5076a6d
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-07-29 14:15:18 -0400

    change ref commands to explicit
    ? becomes Where-Object
    del becomes Remove-Item

--- delete-old-files.ps1
+++ delete-old-files.ps1
@@ -9,9 +9,9 @@ If (!(Test-Path -Path ..\deleteLog.txt -PathType Leaf)){
 
 #Delete files older than x months (user cli args)
 Get-ChildItem $Folder -Recurse -Force -ea 0 |
-? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-($Days))} |
+Where-Object {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-($Days))} |
 ForEach-Object {
-   $_ | del -Force
+   $_ | Remove-Item -Force
    if (!(Test-Path -Path $_.FullName -PathType Leaf)){
         Write-Host "Deleting item " + $_.FullName
         $_.FullName | Out-File .\deleteLog.txt -Append
@@ -20,13 +20,12 @@ ForEach-Object {
 
 #Delete empty folders and subfolders
 Get-ChildItem $Folder -Recurse -Force -ea 0 |
-? {$_.PsIsContainer -eq $True} |
-? {$_.getfiles().count -eq 0} |
+Where-Object {$_.PsIsContainer -eq $True} |
+Where-Object {$_.getfiles().count -eq 0} |
 ForEach-Object {
-    $_ | del -Force
+    $_ | Remove-Item -Force
     if (!(Test-Path -Path $_.FullName -PathType Leaf)){
         Write-Host "Deleting item " + $_.FullName
         $_.FullName | Out-File .\deleteLog.txt -Append
    }
-
 }
